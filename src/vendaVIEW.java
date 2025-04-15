a680b4e28b2efbfd43b5851f20363d228cf3d18e
Andre Cararo <andre_cararo@hotmail.com>
2025-04-14 21:55:02 -0300

    Criada tela de vendas

--- src/vendaVIEW.java
+++ src/vendaVIEW.java
@@ -1,7 +1,4 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
